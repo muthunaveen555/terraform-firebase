7985955279daea4ed7734ba2c422f23ea120c629
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 13:26:39 +0530

    removed the project creation part

--- env/dev/google-beta/main.tf
+++ env/dev/google-beta/main.tf
@@ -1,19 +1,15 @@
-module "google-project" {
-    source = "./../../../modules/google_project"
-}
-
 module "firebase-project" {
     source = "./../../../modules/firebase_project"
-    project-id = module.google-project.google-project-id
+    project-id = "mykubernetes-283719"
 }
 
 module "firebase-webapp" {
     source = "./../../../modules/firebase_webapp"
-    project-id = module.google-project.google-project-id
+    project-id = "mykubernetes-283719"
 }
 
 module "cloud-storage" {
     source = "./../../../modules/cloud_storage"
     web-app-id = module.firebase-webapp.app-id
-    project-id = module.google-project.google-project-id
+    project-id = "mykubernetes-283719"
 }
