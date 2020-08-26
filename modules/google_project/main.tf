d0d141cda0cf193e9fea535018bb8cfddd5505f6
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 13:10:23 +0530

    changed the project name

--- modules/google_project/main.tf
+++ modules/google_project/main.tf
@@ -1,6 +1,6 @@
 resource "google_project" "default" {
     provider = google-beta
-    project_id = "terraform-firebase"
-    name       = "terraform-firebase"
+    project_id = "dream11-project"
+    name       = "dream11-project"
 }
 
