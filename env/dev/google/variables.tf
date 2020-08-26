4e135b4e84960b6928c3ab5ece9a15b21b109817
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 14:08:04 +0530

    addedd loadbalancer, cloud build, cloud fun

---
+++ env/dev/google/variables.tf
@@ -0,0 +1,41 @@
+variable "branch_name"{
+    default="function-1"
+}
+variable "repo_name"{
+    default="pe-mock-project-cf"
+}
+variable "func_name"{
+    default="function-1"
+}
+variable "entrypoint"{
+    default="hello_world"
+}
+variable "runtime_env"{
+    default="python37"
+}
+variable "http_trigger"{
+    type = bool
+    default=true
+}
+variable "functionality"{
+    default="Defines the fucntionality of Cloud function"
+}
+variable "bucket_name"{
+    default="firebase-webapp"
+}
+variable "backend_bucket_name"{
+    default="firebase-webapp"
+}
+variable "cdn_option"{
+    type=bool
+    default=false
+}
+variable "sourcedir"{
+    default="./"
+}
+variable "target_proxy_name"{
+    default="pe-mock-http-proxy"
+}
+variable "forwarding_rule_name"{
+    default="pe-mock-forwarding-rule"
+}
--- modules/cloud_build/variables.tf
+++ modules/cloud_build/variables.tf
@@ -1,6 +1,9 @@
 variable "branch_name"{
+    type = ""
     default="function-1"
 }
+
 variable "repo_name"{
+    type = ""
     default="pe-mock-project-cf"
 }
