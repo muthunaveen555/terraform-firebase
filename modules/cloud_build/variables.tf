5f1051dc0555e838a5634faf6c291af092f840fb
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 14:08:42 +0530

    addedd loadbalancer, cloud build, cloud fun

--- modules/cloud_build/variables.tf
+++ modules/cloud_build/variables.tf
@@ -1,9 +1,9 @@
 variable "branch_name"{
-    type = ""
+    type = string
     default="function-1"
 }
 
 variable "repo_name"{
-    type = ""
+    type = string
     default="pe-mock-project-cf"
 }
