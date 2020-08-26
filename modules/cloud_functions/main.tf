42ef089da5845697cc229281b24ea182905e4f60
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 17:22:01 +0530

    bucket name changed

--- modules/cloud_functions/main.tf
+++ modules/cloud_functions/main.tf
@@ -7,7 +7,7 @@ resource "google_cloudfunctions_function" "myCloudFunction" {
     trigger_http              = var.http_trigger
 
     source_repository  {
-        url = "https://source.developers.google.com/p/iron-area-283612/r/pe-mock-project-cf/moveable-aliases/function-1/"
+        url = "https://source.developers.google.com/projects/iron-area-283612/repos/pe-mock-project-cf/moveable-aliases/function-1/paths/*"
     }
 
     labels = {
