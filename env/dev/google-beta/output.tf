70ad99c29226534a4de96cc8ea217e3dcc54ffd5
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 13:27:29 +0530

    removed the project creation part

--- env/dev/google-beta/output.tf
+++ env/dev/google-beta/output.tf
@@ -2,8 +2,3 @@ output "app-id" {
     value = module.firebase-webapp.app-id
     description = "app id of the web app"
 }
-
-output "google-project-id" {
-    description = "project id of the google project we created"
-    value = module.google-project.google-project-id
-}
