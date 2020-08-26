a705694dc7d030542d985d820deeed687cc456d0
Naveen kumar <muthunaveen555@gmail.com>
2020-08-27 00:06:34 +0530

    terraform fmt applied

--- modules/cloud_build/main.tf
+++ modules/cloud_build/main.tf
@@ -1,8 +1,8 @@
 resource "google_cloudbuild_trigger" "myTrigger" {
-  count = var.number_of_triggers
+  count    = var.number_of_triggers
+  filename = "cloudbuild.yaml"
   trigger_template {
     branch_name = var.branch_list[count.index]
     repo_name   = var.repo_name
   }
-  filename = "cloudbuild.yaml"
 }
--- modules/cloud_build/variables.tf
+++ modules/cloud_build/variables.tf
@@ -1,20 +1,20 @@
-variable "branch_name"{
-    type = string
-    default="function-1"
+variable "branch_name" {
+  type    = string
+  default = "function-1"
 }
 
-variable "repo_name"{
-    type = string
-    default="pe-mock-project-cf"
+variable "repo_name" {
+  type    = string
+  default = "pe-mock-project-cf"
 }
 
 variable "branch_list" {
-    type = list(string)
-    default = ["function-1","function-2"]
-    description = "(optional) describe your variable"
+  type        = list(string)
+  default     = ["function-1", "function-2"]
+  description = "(optional) describe your variable"
 }
 
 variable "number_of_triggers" {
-    type = number
-    description = "(optional) describe your variable"
+  type        = number
+  description = "(optional) describe your variable"
 }
