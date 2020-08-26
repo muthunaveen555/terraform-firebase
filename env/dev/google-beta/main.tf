15c37a3c296cd854e0ba5263bcf921a0920b341e
Naveen kumar <muthunaveen555@gmail.com>
2020-08-26 15:48:21 +0530

    project changed

--- env/dev/google-beta/main.tf
+++ env/dev/google-beta/main.tf
@@ -5,11 +5,11 @@
 
 module "firebase-webapp" {
     source = "./../../../modules/firebase_webapp"
-    project-id = "mykubernetes-283719"
+    project-id = "iron-area-283612"
 }
 
 module "cloud-storage" {
     source = "./../../../modules/cloud_storage"
     web-app-id = module.firebase-webapp.app-id
-    project-id = "mykubernetes-283719"
+    project-id = "iron-area-283612"
 }
