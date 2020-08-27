06bdff91a62b4613d8c79478285911412c1e9258
Naveen kumar <muthunaveen555@gmail.com>
2020-08-27 12:47:39 +0530

    changed the output

--- env/dev/google/output.tf
+++ env/dev/google/output.tf
@@ -1,3 +1,7 @@
+output "function_names" {
+  value = module.cloud-function.function_names
+}
+
 output "function_urls" {
   value = module.cloud-function.function_urls
 }
--- modules/cloud_functions/output.tf
+++ modules/cloud_functions/output.tf
@@ -1,3 +1,7 @@
 output "function_urls" {
   value = google_cloudfunctions_function.myCloudFunction.*.https_trigger_url
 }
+
+output "function_names" {
+  value = google_cloudfunctions_function.myCloudFunction.*.id
+}
