91a08e50fb1d7f7d9c5ced193d11f823db1ce799
Naveen kumar <muthunaveen555@gmail.com>
2020-08-27 13:33:08 +0530

    commiting to check csr

--- env/dev/google/main.tf
+++ env/dev/google/main.tf
@@ -1,20 +1,16 @@
+#cloud build module
 module "cloud-build" {
   number_of_triggers = length(var.branch_list)
   source             = "./../../../modules/cloud_build"
 }
 
-# module "cloud-build-bucket" {
-#   source           = "./../../../modules/cloud_build"
-#   var.branch_name  = "master"
-#   var.repo_name    = "pe-mock-project-gcs"
-# }
-
-
+#cloud function module
 module "cloud-function" {
   number_of_functions = length(var.function_list)
   source              = "./../../../modules/cloud_functions"
 }
 
+#load balancer module
 module "load_balancer" {
   source = "./../../../modules/load_balancer"
 }
